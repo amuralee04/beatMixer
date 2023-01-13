f0d711159d799fa013cfa3942bc27c9648a1887d
amuralee04 <114322925+amuralee04@users.noreply.github.com>
2023-01-12 22:39:37 -0500

    Added server-side func to handle API reqs

---
+++ presethandler.js
@@ -0,0 +1,36 @@
+const presets = require('./presets');
+
+const getPreset = (ind) => {
+    return presets[ind] || null;
+}
+
+const createOrUpdatePreset = (index, array) => {
+    if (!presets[index]) {
+        return;
+    }
+
+    presets[index] = array;
+    return presets[index];
+}
+
+const presetHandler = (method, index, newPresetArray) => {
+    if (method === 'GET') {
+        let preset = getPreset(index);
+        if (preset) {
+            return [200, preset];
+        } else {
+            return [404];
+        }
+    } else if (method === 'PUT') {
+        const newPreset = createOrUpdatePreset(index, newPresetArray);
+        if (newPreset) {
+            return [200, newPreset];
+        } else {
+            return [404];
+        }
+    } else {
+        return [400];
+    }
+}
+
+module.exports = presetHandler;
